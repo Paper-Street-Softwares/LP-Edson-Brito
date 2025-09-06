08bd30b1efb1b5dec7a1de1b90bd35c7ed49def9
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-06 00:16:26 -0300

    fix: previewIcon, favico, imagens e logo em webp

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,17 @@ export const theme = {
     colors: {
       // Principais
 
-      primary: "#058cd7",
-      buttonColor: "#058cd7",
-      minititleDark: "#058cd7", //bg minitag de seções claras
-      minititle: "#058cd7", //bg minitag de seções escuras
+      primary: "#886F3C",
+      buttonColor: "#886F3C",
+      minititleDark: "#886F3C", //bg minitag de seções claras
+      minititle: "#886F3C", //bg minitag de seções escuras
 
       secondary: "#000000",
 
       // Fundos de Seção
 
       bgSectionLight: "#FFFFFF",
-      bgSectionDark: "#990000",
+      bgSectionDark: "#2F3545",
       bgSectionOpacityLight: "#F7F7F7",
       bgFixedLight: "#E8E8E8",
       bgSectionOpacityDark: "#111111",
